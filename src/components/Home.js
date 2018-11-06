db9e706bcfc163eebddba64dfb38b47c585dd413
Justin <jdlow2@gmail.com>
2018-11-06 10:23:05 -0500

    favicons use camelCase instead of snake_case

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Object } from 'core-js';
-import running_favicon from './../img/running_favicon.png';
-import stopped_favicon from './../img/stopped_favicon.png';
+import runningFavicon from './../img/running_favicon.png';
+import stoppedFavicon from './../img/stopped_favicon.png';
 
 import Clock from './Clock';
 import { MyButton, ResetButton } from './Button';
@@ -28,7 +28,7 @@ class Home extends React.Component {
   componentDidMount() {
     // this works: document.getElementById('favicon').href = 'http://www.cutestpaw.com/wp-content/uploads/2016/02/s-Kitty-yoga.%281%29.jpg'
     this.favicon = document.getElementById('favicon');
-    this.favicon.href = stopped_favicon;
+    this.favicon.href = stoppedFavicon;
     Object.keys(this.state).forEach(key => {
       if (localStorage.getItem(key)) {
         this.setState({ [key]: JSON.parse(localStorage.getItem(key)) });
@@ -90,7 +90,7 @@ class Home extends React.Component {
 
   toggleClock = () => {
     if (!this.state.running) {
-      this.favicon.href = running_favicon;
+      this.favicon.href = runningFavicon;
       if (this.state.jobStartTime === null) {
         this.setState({ jobStartTime: Date() });
         localStorage.setItem('jobStartTime', JSON.stringify(Date()));
@@ -108,7 +108,7 @@ class Home extends React.Component {
       }, 1000);
       this.setState({ clockInt: interval });
     } else {
-      this.favicon.href = stopped_favicon;
+      this.favicon.href = stoppedFavicon;
       clearInterval(this.state.clockInt);
       this.setState({ currentTimer: 0 });
     }
